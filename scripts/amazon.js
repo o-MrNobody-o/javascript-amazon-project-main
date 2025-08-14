ea8f4890a638e40aecc90ce80d47806b2306a081
o-MrNobody-o <achrefbouali335@gmail.com>
2025-08-14 07:47:15 +0100

    h2

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -22,7 +22,7 @@ products.forEach((product) =>
             </div>
 
             <div class="product-price">
-                $${(product.priceCent / 100).toFixed(2)}
+                $${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -47,7 +47,8 @@ products.forEach((product) =>
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
                 Add to Cart
             </button>
         </div>
@@ -55,3 +56,36 @@ products.forEach((product) =>
 })
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML ;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+
+        cart.forEach( (item) => {
+            if (productId === item.productId)
+            {
+                matchingItem = item ;
+            }
+        });
+
+        if  (matchingItem) 
+        {
+            matchingItem.quantity += 1 ;
+        }
+        else
+        {
+            cart.push(
+            {
+            productId : productId ,
+            quantity : 1 
+            }
+        );
+        }
+
+
+        
+        console.log(cart);
+    })
+})
