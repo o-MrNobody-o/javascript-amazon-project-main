4995c5fc7ae235e4963f00bf9ec8250dea3f2ff6
o-MrNobody-o <achrefbouali335@gmail.com>
2025-09-18 07:36:42 +0100

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./chekout/orderSummary.js";
 import { renderPaymentSummary } from "./chekout/paymentSummary.js";
+import '../data/cart-oop.js'
 
 renderPaymentSummary();
 renderOrderSummary();
